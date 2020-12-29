4f057c82976cb7ed24f7e30e6f81a4c982369a1b
Zymantasdan <z.dansevicius@gmail.com>
2020-12-29 14:31:07 +0200

    3

---
+++ js/kintamasis.js
@@ -0,0 +1,14 @@
+const sausis = "Sausis";
+const vasaris = "Vasaris";
+const kovas = "Kovas";
+const balandis = "Balandis";
+const geguze = "Gegužė";
+const birzelis = "Birželis";
+const liepa = "Liepa";
+const rugpjutis = "Rugpjūtis";
+const rugsejis = "Rugsėjis";
+const spalis = "Spalis";
+const lapkritis = "Lapkritis";
+const gruodis = "Gruodis";
+
+
